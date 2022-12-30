4990cfe74ae4fbde187f983d99552dccade9ca28
Magara  Etchien N'gouan <120406739+Magara1@users.noreply.github.com>
2022-12-30 11:00:54 +0000

    Update exercice2.js

--- exercice2.js
+++ exercice2.js
@@ -2,4 +2,4 @@ const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
 
 //1. Access and then console.log “Oranges”.
 
-console.log(moreFruits[1][1][2])
+console.log(moreFruits[1][1][0])
